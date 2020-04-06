a2a250f3f016a83b0cb77af4253982606769a397
abmohamm <mohd.92java@gmail.com>
2020-04-06 09:14:25 -0400

    Added compose-message component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 
 const appRoutes: Routes = [
                          { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+                         { path: 'compose', component: ComposeMessageComponent, outlet: 'popup' },
                          { path: '**', component: PageNotFoundComponent }
                        ];
 
